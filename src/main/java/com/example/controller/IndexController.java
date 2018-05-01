a16b1b5f9bf0746ee8d8f2f2bde57785252cc2c3
Toni Glogovski <toni_glogovski@abv.bg>
2018-05-01 13:46:21 +0300

    edit IndexController - session

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -1,17 +1,24 @@
 package com.example.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value="/index")
+@RequestMapping(value = "/index")
 public class IndexController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String sayHello(Model model) {
+	public String sayHello(Model model, HttpServletRequest request, HttpServletResponse response) {
+
+		if ((request.getSession(false) != null) && (request.getAttribute("id") != null)) {
+			long id = (long) request.getSession().getAttribute("id");
+		}
 		return "index";
-	}	
+	}
 
 }
