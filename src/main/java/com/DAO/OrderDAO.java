e9fcda63ad79cde775f413c0bf664810efefbe05
Toni Glogovski <toni_glogovski@abv.bg>
2018-05-09 15:01:32 +0300

    add more UserTests and CategoryTest

--- src/main/java/com/DAO/OrderDAO.java
+++ src/main/java/com/DAO/OrderDAO.java
@@ -15,45 +15,43 @@ import com.model.Order;
 import com.model.User;
 
 @Component
-public class OrderDAO implements IOrderDAO{
+public class OrderDAO implements IOrderDAO {
 
 	private static final String INSERT_ADDRESS = "insert into address(first_name, last_name, phone, city, post_code,"
-			+ "street, street_number, block, enterance, floor, appartment, description) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"; 
-	private static final String INSERT_ORDER = "insert INTO `order`(users_id, address_id) values (?, ?)"; 
-	private static final String INSERT_ITEMS_HAS_ORDER = "insert into items_has_order(items_id, order_id, quanitity) values (?, ?, ?)"; 
+			+ "street, street_number, block, enterance, floor, appartment, description) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	private static final String INSERT_ORDER = "insert INTO `order`(users_id, address_id) values (?, ?)";
+	private static final String INSERT_ITEMS_HAS_ORDER = "insert into items_has_order(items_id, order_id, quanitity) values (?, ?, ?)";
 	private static final String FIND_ADDRESS = "SELECT * from address where first_name = ? AND last_name = ? AND phone = ? AND city =? AND post_code = ? AND street= ?";
 	private Connection conn;
-	
-	private OrderDAO() throws SQLException{
+
+	private OrderDAO() throws SQLException {
 		this.conn = DBConnection.getInstance().getConnection();
 	}
-	
+
 	@Override
 	public void makeOrder(Order order) {
 		try {
-			
+
 			long addressId = 0;
 			int orderId = 0;
-			//check if adress exists
-		
+			// check if adress exists
+
 			PreparedStatement stmt = conn.prepareStatement(FIND_ADDRESS);
-			
-			
+
 			stmt.setString(1, order.getOrdererFirstName());
 			stmt.setString(2, order.getOrdererLastName());
 			stmt.setString(3, order.getOrdererPhone());
 			stmt.setString(4, order.getOrdererCity());
 			stmt.setString(5, order.getOrdererPostCode());
 			stmt.setString(6, order.getOrdererStreet());
-			
+
 			ResultSet rs = stmt.executeQuery();
-			
-			if(rs.next()){
-				 addressId = rs.getInt("id");
-				
-				
-			} else{
-				PreparedStatement stmt2 = conn.prepareStatement(INSERT_ADDRESS,Statement.RETURN_GENERATED_KEYS);
+
+			if (rs.next()) {
+				addressId = rs.getInt("id");
+
+			} else {
+				PreparedStatement stmt2 = conn.prepareStatement(INSERT_ADDRESS, Statement.RETURN_GENERATED_KEYS);
 				stmt2.setString(1, order.getOrdererFirstName());
 				stmt2.setString(2, order.getOrdererLastName());
 				stmt2.setString(3, order.getOrdererPhone());
@@ -61,88 +59,70 @@ public class OrderDAO implements IOrderDAO{
 				stmt2.setString(5, order.getOrdererPostCode());
 				stmt2.setString(6, order.getOrdererStreet());
 				setInteger(7, order.getOrdererStreetNumber(), stmt2);
-			
 				setString(8, order.getOrdererBlock(), stmt2);
 				setString(9, order.getOrdererEnterance(), stmt2);
-				
 				setInteger(10, order.getOrdererFloor(), stmt2);
-				
 				setString(11, order.getOrdererAppartment(), stmt2);
 				setString(12, order.getDescription(), stmt2);
 
-				
 				synchronized (this) {
 					stmt2.executeUpdate();
 				}
 				ResultSet rs2 = stmt2.getGeneratedKeys();
-				
+
 				/// try get address throw address exception
-				if(rs2.next()) {
-					addressId= rs2.getLong(1);
+				if (rs2.next()) {
+					addressId = rs2.getLong(1);
 				}
 				stmt2.close();
 			}
-			
-			
-			
-			
-			
 			PreparedStatement stmt3 = conn.prepareStatement(INSERT_ORDER);
 			stmt3.setInt(1, order.getUserId());
 			stmt3.setLong(2, addressId);
-			
+
 			synchronized (this) {
 				stmt3.executeUpdate();
 			}
 			ResultSet rs3 = stmt3.getGeneratedKeys();
-			if(rs3.next()) {
-				orderId= rs3.getInt(1);
+			if (rs3.next()) {
+				orderId = rs3.getInt(1);
 			}
 			stmt3.close();
-			
-			
-			
-			Map<Integer,Integer> items = order.getItems();
-			for(Integer itemId : items.keySet()){
+
+			Map<Integer, Integer> items = order.getItems();
+			for (Integer itemId : items.keySet()) {
 				int quantity = items.get(itemId).intValue();
 				PreparedStatement stmt4 = conn.prepareStatement(INSERT_ITEMS_HAS_ORDER);
 				stmt4.setInt(1, itemId.intValue());
 				stmt4.setInt(2, orderId);
 				stmt4.setInt(3, quantity);
-				
+
 				synchronized (this) {
 					stmt4.executeUpdate();
 				}
 				stmt4.close();
 			}
-			
-			
+
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-	}
-	
-	
 
-	private void setInteger(int stmtIndex,Integer integer, PreparedStatement stmt) throws SQLException {
-		if(integer == null ){
+	}
+
+	private void setInteger(int stmtIndex, Integer integer, PreparedStatement stmt) throws SQLException {
+		if (integer == null) {
 			stmt.setNull(stmtIndex, java.sql.Types.INTEGER);
-		}else{
+		} else {
 			stmt.setInt(stmtIndex, integer);
 		}
 	}
-	
-	private void setString(int stmtIndex,String string, PreparedStatement stmt) throws SQLException {
-		if(string.length() == 0){
+
+	private void setString(int stmtIndex, String string, PreparedStatement stmt) throws SQLException {
+		if (string.length() == 0) {
 			stmt.setString(stmtIndex, null);
-		}else{
+		} else {
 			stmt.setString(stmtIndex, string);
 		}
 	}
-	
 
 }
--- src/main/java/com/tests/UserTests.java
+++ src/main/java/com/tests/UserTests.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,12 +29,42 @@ public class UserTests {
 	UserDAO dao;
 	
 	@Test
-	public void test() throws SQLException, UserException {
+	public void testBadPassword() throws SQLException, UserException {
 		User user = new User("toni@abv.bg", "1234");
 		Boolean  loginCheck = dao.login(user.getEmail(), user.getPassword());
 		
 		
-		assertThat(loginCheck, is(false));
+		assertFalse(loginCheck);
 	}
-
+	
+	@Test
+	public void testBadEmail() throws SQLException, UserException {
+		User user = new User("toniiiii@abv.bg", "123");
+		Boolean  loginCheck = dao.login(user.getEmail(), user.getPassword());
+		
+		
+		assertFalse(loginCheck);
+	}
+	
+//	@Test
+//	public void testRegister() throws SQLException, UserException {
+//		LocalDate localDate = LocalDate.now();
+//		User user = new User("Anton", "Petrov", "anton@abv.bg", "123", localDate);
+//
+//		dao.register(user);
+//		Boolean loginCheck = dao.login(user.getEmail(), user.getPassword());
+//		assertTrue(loginCheck);
+//	}
+	
+	
+	@Test(expected = UserException.class)
+	public void testUser() throws UserException, SQLException {
+		LocalDate localDate = LocalDate.now();
+		User user = new User("Ivan", "Petrov", "ivanov@abv.bg", "123", localDate);
+		dao.register(user);
+		Boolean checkUser = dao.checkIfUserExists(user);
+		
+		assertFalse(checkUser);
+	}
+	
 }
